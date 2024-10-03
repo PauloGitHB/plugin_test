02016b7072bb63094872698db387dd3103af9b76
Paul Levasseur <paul.levasseur@epita.fr>
2024-10-03 11:04:36 +0200

    sintering method

--- src/plugin_test/normalizers/normalizer.py
+++ src/plugin_test/normalizers/normalizer.py
@@ -1,27 +1,27 @@
-from typing import (
-    TYPE_CHECKING,
-)
+# from typing import (
+#     TYPE_CHECKING,
+# )
 
-if TYPE_CHECKING:
-    pass
+# if TYPE_CHECKING:
+#     pass
 
-from nomad.config import config
-from nomad.normalizing import Normalizer
+# from nomad.config import config
+# from nomad.normalizing import Normalizer
 
-configuration = config.get_plugin_entry_point(
-    'plugin_test.normalizers:normalizer_entry_point'
-)
+# configuration = config.get_plugin_entry_point(
+#     'plugin_test.normalizers:normalizer_entry_point'
+# )
 
-class MyNormalizer(Normalizer):
-    def normalize(self, archive, logger):
-        logger.info('Normalizing data')
+# class MyNormalizer(Normalizer):
+#     def normalize(self, archive, logger):
+#         logger.info('Normalizing data')
 
-        if not archive.metadata.instrument:
-            archive.metadata.instrument = 'Unknown Instrument'
-            logger.warning('Instrument not provided, set to "Unknown Instrument".')
+#         if not archive.metadata.instrument:
+#             archive.metadata.instrument = 'Unknown Instrument'
+#             logger.warning('Instrument not provided, set to "Unknown Instrument".')
 
-        for signal in archive.signals:
-            if not hasattr(signal, 'unit'):
-                signal.unit = 'arbitrary_unit'
-                logger.info(f'Unit for {signal.name} set to "arbitrary_unit"')
+#         for signal in archive.signals:
+#             if not hasattr(signal, 'unit'):
+#                 signal.unit = 'arbitrary_unit'
+#                 logger.info(f'Unit for {signal.name} set to "arbitrary_unit"')
 
--- src/plugin_test/parsers/__init__.py
+++ src/plugin_test/parsers/__init__.py
@@ -1,18 +1,18 @@
-from nomad.config.models.plugins import ParserEntryPoint
-from pydantic import Field
+# from nomad.config.models.plugins import ParserEntryPoint
+# from pydantic import Field
 
 
-class MyParserEntryPoint(ParserEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+# class MyParserEntryPoint(ParserEntryPoint):
+#     parameter: int = Field(0, description='Custom configuration parameter')
 
-    def load(self):
-        from plugin_test.parsers.parser import MyParser
+#     def load(self):
+#         from plugin_test.parsers.parser import MyParser
 
-        return MyParser(**self.dict())
+#         return MyParser(**self.dict())
 
 
-parser_entry_point = MyParserEntryPoint(
-    name='MyParser',
-    description='New parser entry point configuration.',
-    mainfile_name_re='.*\.txt',
-)
+# parser_entry_point = MyParserEntryPoint(
+#     name='MyParser',
+#     description='New parser entry point configuration.',
+#     mainfile_name_re='.*\.txt',
+# )
