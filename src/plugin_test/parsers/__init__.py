8d73e8b7e669017f86b760914440b6f89a6f6259
Paul Levasseur <paul.levasseur@epita.fr>
2024-10-08 16:26:46 +0200

    v1

--- src/plugin_test/parsers/__init__.py
+++ src/plugin_test/parsers/__init__.py
@@ -2,16 +2,16 @@ from nomad.config.models.plugins import ParserEntryPoint
 from pydantic import Field
 
 
-class MyParserEntryPoint(ParserEntryPoint):
+class OscilloscopeEntryPoint(ParserEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from plugin_test.parsers.parser import MyParser
+        from plugin_test.parsers.parser import OscilloscopeParser
 
-        return MyParser(**self.dict())
+        return OscilloscopeParser(**self.dict())
 
 
-parser_entry_point = MyParserEntryPoint(
+parser_entry_point = OscilloscopeEntryPoint(
     name='MyParser',
     description='New parser entry point configuration.',
     mainfile_name_re='.*\.txt',
